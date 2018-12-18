00c5a940a2bca428e8c243539b0f60df232bbf89
yige <a@wyr.me>
2018-12-18 14:53:48 +0800

    standard

--- pay.js
+++ pay.js
@@ -9,22 +9,19 @@ const toQueryString = (obj) => Object.keys(obj)
   .filter(key => key !== 'sign' && obj[key] !== undefined && obj[key] !== '')
   .sort()
   .map(key => {
-    if (/^http(s)?:\/\//.test(obj[key]))
-      return key + '=' + encodeURI(obj[key])
-    else
-      return key + '=' + obj[key]
+    if (/^http(s)?:\/\//.test(obj[key])) { return key + '=' + encodeURI(obj[key]) } else { return key + '=' + obj[key] }
   })
   .join('&')
 
 const md5 = (str, encoding = 'utf8') => crypto.createHash('md5').update(str, encoding).digest('hex')
 
 let params = {
-  "body": "合同:小黄人测试",
-  "attach": "合同:小黄人测试",
-  "total_fee": 1,
-  "out_trade_no": "FF6F8FB0114D4FAFA3B562BD89B897E8",
-  "openid": "o7LFAwaFdmKH217MddWDDyYuv7Ms",
-  "mchid": "1519390541"
+  'body': '合同:小黄人测试',
+  'attach': '合同:小黄人测试',
+  'total_fee': 1,
+  'out_trade_no': 'FF6F8FB0114D4FAFA3B562BD89B897E8',
+  'openid': 'o7LFAwaFdmKH217MddWDDyYuv7Ms',
+  'mchid': '1519390541'
 }
 
 params = toQueryString(params)
